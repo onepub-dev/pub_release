b09a310910ec4d9024c012e8fc60ed4e86e8cbc6
Brett Sutton <bsutton@noojee.com.au>
2020-09-07 11:16:55 +1000

    Disabled the github release logic until we get the new version of github.dart

--- lib/src/git_hub_release.dart
+++ lib/src/git_hub_release.dart
@@ -1,80 +1,80 @@
-import 'dart:io';
+//import 'dart:io';
 
-import 'package:dcli/dcli.dart';
-import 'package:github/github.dart';
-import 'package:meta/meta.dart';
+//import 'package:dcli/dcli.dart';
+// import 'package:github/github.dart';
+//import 'package:meta/meta.dart';
 
 class GitHubRelease {
-  final String username;
-  final String apiToken;
-  final String owner;
-  final String repository;
+  // final String username;
+  // final String apiToken;
+  // final String owner;
+  // final String repository;
 
-  GitHub _github;
+  // GitHub _github;
 
-  RepositorySlug _repositorySlug;
+  // RepositorySlug _repositorySlug;
 
-  RepositoriesService _repoService;
+  // RepositoriesService _repoService;
 
-  GitHubRelease(
-      {@required this.username,
-      @required this.apiToken,
-      @required this.owner,
-      @required this.repository});
+  // GitHubRelease(
+  //     {@required this.username,
+  //     @required this.apiToken,
+  //     @required this.owner,
+  //     @required this.repository});
 
-  void auth() {
-    //var github = GitHub(auth: findAuthenticationFromEnvironment());
-    _github = GitHub(auth: Authentication.basic(username, apiToken));
+  // void auth() {
+  //   //var github = GitHub(auth: findAuthenticationFromEnvironment());
+  //   _github = GitHub(auth: Authentication.basic(username, apiToken));
 
-    _repositorySlug = RepositorySlug(owner, repository);
+  //   _repositorySlug = RepositorySlug(owner, repository);
 
-    _repoService = RepositoriesService(_github);
-  }
+  //   _repoService = RepositoriesService(_github);
+  // }
 
-  ///
-  /// Creates a git hub release and returns the created release.
-  ///
-  /// Throws a GitHubException if the given tagName already exists.
-  Release release({@required String tagName}) {
-    var createRelease = CreateRelease(tagName);
+  // ///
+  // /// Creates a git hub release and returns the created release.
+  // ///
+  // /// Throws a GitHubException if the given tagName already exists.
+  // Release release({@required String tagName}) {
+  //   var createRelease = CreateRelease(tagName);
 
-    var release =
-        waitForEx(_repoService.getReleaseByTagName(_repositorySlug, tagName));
-    if (release == null) {
-      release = waitForEx<Release>(
-          _repoService.createRelease(_repositorySlug, createRelease));
-    } else {
-      throw GitHubException('A release with tagName $tagName already exists');
-    }
+  //   var release =
+  //       waitForEx(_repoService.getReleaseByTagName(_repositorySlug, tagName));
+  //   if (release == null) {
+  //     release = waitForEx<Release>(
+  //         _repoService.createRelease(_repositorySlug, createRelease));
+  //   } else {
+  //     throw GitHubException('A release with tagName $tagName already exists');
+  //   }
 
-    return release;
-  }
+  //   return release;
+  // }
 
-  Release getByTagName({@required String tagName}) {
-    return waitForEx(
-        _repoService.getReleaseByTagName(_repositorySlug, tagName));
-  }
+  // Release getByTagName({@required String tagName}) {
+  //   return waitForEx(
+  //       _repoService.getReleaseByTagName(_repositorySlug, tagName));
+  // }
 
-  void attachAssetFromFile(
-      {Release release,
-      String assetName,
-      String assetLabel,
-      String assetPath,
-      String mimeType}) {
-    var assetData = File(assetPath).readAsBytesSync();
+  // void attachAssetFromFile(
+  //     {Release release,
+  //     String assetName,
+  //     String assetLabel,
+  //     String assetPath,
+  //     String mimeType}) {
+  //   var assetData = File(assetPath).readAsBytesSync();
 
-    var installAsset = CreateReleaseAsset(
-      name: assetName,
-      contentType: mimeType,
-      assetData: assetData,
-      label: assetLabel,
-    );
-    waitForEx(_repoService.uploadReleaseAssets(release, [installAsset]));
-  }
+  //   var installAsset = CreateReleaseAsset(
+  //     name: assetName,
+  //     contentType: mimeType,
+  //     assetData: assetData,
+  //     label: assetLabel,
+  //   );
+  //   waitForEx(_repoService.uploadReleaseAssets(release, [installAsset]));
+  // }
 
-  void deleteRelease(Release release) {
-    waitForEx(_repoService.deleteRelease(_repositorySlug, release));
-  }
+  // void deleteRelease(Release release) {
+  //   waitForEx(_repoService.deleteRelease(_repositorySlug, release));
+  // }
 }
 
 class GitHubException implements Exception {
--- test/git_hub_release_test.dart
+++ test/git_hub_release_test.dart
@@ -1,59 +1,59 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:dcli/dcli.dart';
-import 'package:mime/mime.dart';
-import 'package:pub_release/src/git_hub_release.dart';
+// import 'package:dcli/dcli.dart';
+// import 'package:mime/mime.dart';
+// import 'package:pub_release/src/git_hub_release.dart';
 
 void main() {
-  var ghr = GitHubRelease(
-      username: 'bsutton',
-      apiToken: 'XXXXX',
-      owner: 'bsutton',
-      repository: 'dcli');
+//   var ghr = GitHubRelease(
+//       username: 'bsutton',
+//       apiToken: 'XXXXX',
+//       owner: 'bsutton',
+//       repository: 'dcli');
 
-  ghr.auth();
+//   ghr.auth();
 
-  var tagName = '0.0.3-${Platform.operatingSystem}';
+//   var tagName = '0.0.3-${Platform.operatingSystem}';
 
-  /// update latest tag to point to this new tag.
-  var old = ghr.getByTagName(tagName: tagName);
-  if (old != null) {
-    print('replacing release $tagName');
-    ghr.deleteRelease(old);
-  }
+//   /// update latest tag to point to this new tag.
+//   var old = ghr.getByTagName(tagName: tagName);
+//   if (old != null) {
+//     print('replacing release $tagName');
+//     ghr.deleteRelease(old);
+//   }
 
-  // 'dcli compile -o $HOME/git/dcli/bin/dcli_install.dart'.run;
-  var exe = '$HOME/git/dcli/bin/dcli_install';
-  print('Creating release: $tagName');
-  var release = ghr.release(tagName: tagName);
+//   // 'dcli compile -o $HOME/git/dcli/bin/dcli_install.dart'.run;
+//   var exe = '$HOME/git/dcli/bin/dcli_install';
+//   print('Creating release: $tagName');
+//   var release = ghr.release(tagName: tagName);
 
-// 'application/vnd.microsoft.portable-executable'
-  print('Sending Asset  $exe');
-  ghr.attachAssetFromFile(
-    release: release,
-    assetPath: exe,
-    assetName: 'dcli_install',
-    // assetLabel: 'DCli installer',
-    mimeType: lookupMimeType('$exe.exe'),
-  );
-  print('send complete');
+// // 'application/vnd.microsoft.portable-executable'
+//   print('Sending Asset  $exe');
+//   ghr.attachAssetFromFile(
+//     release: release,
+//     assetPath: exe,
+//     assetName: 'dcli_install',
+//     // assetLabel: 'DCli installer',
+//     mimeType: lookupMimeType('$exe.exe'),
+//   );
+//   print('send complete');
 
-  /// update latest tag to point to this new tag.
-  var latest = ghr.getByTagName(tagName: 'latest-${Platform.operatingSystem}');
-  if (latest != null) {
-    ghr.deleteRelease(latest);
-  }
+//   /// update latest tag to point to this new tag.
+//   var latest = ghr.getByTagName(tagName: 'latest-${Platform.operatingSystem}');
+//   if (latest != null) {
+//     ghr.deleteRelease(latest);
+//   }
 
-  release = ghr.release(tagName: 'latest-${Platform.operatingSystem}');
+//   release = ghr.release(tagName: 'latest-${Platform.operatingSystem}');
 
-// 'application/vnd.microsoft.portable-executable'
-  print('Sending Asset');
-  ghr.attachAssetFromFile(
-    release: release,
-    assetPath: exe,
-    assetName: 'dcli_install',
-    // assetLabel: 'DCli installer',
-    mimeType: lookupMimeType('$exe.exe'),
-  );
-  print('send complete');
+// // 'application/vnd.microsoft.portable-executable'
+//   print('Sending Asset');
+//   ghr.attachAssetFromFile(
+//     release: release,
+//     assetPath: exe,
+//     assetName: 'dcli_install',
+//     // assetLabel: 'DCli installer',
+//     mimeType: lookupMimeType('$exe.exe'),
+//   );
+//   print('send complete');
 }
