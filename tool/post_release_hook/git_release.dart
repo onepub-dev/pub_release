83f3ef8ec1eb859f173588733f4b0a12996e2a9d
Brett Sutton <bsutton@noojee.com.au>
2020-10-22 09:37:13 +1100

    moved credentials into settings.yaml.

---
+++ tool/post_release_hook/git_release.dart
@@ -0,0 +1,18 @@
+#! /usr/bin/env dcli
+
+import 'package:dcli/dcli.dart';
+import 'package:settings_yaml/settings_yaml.dart';
+
+void main(List<String> args) {
+  var project = DartProject.current;
+
+  var pathToSettings = join(project.pathToProjectRoot, 'tool', 'post_release_hook', 'settings.config');
+  var settings = SettingsYaml.load(pathToSettings: pathToSettings);
+  var username = settings['username'] as String;
+  var apiToken = settings['apiToken'] as String;
+  var owner = settings['owner'] as String;
+  var repository = settings['repository'] as String;
+
+  'github_release -u $username --apiToken $apiToken --owner $owner --repository $repository --suffix linux'
+      .start(workingDirectory: Script.current.pathToProjectRoot);
+}
