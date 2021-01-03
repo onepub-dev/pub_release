315324e2221ddf3e7307e1bf5ea90196eff3c63c
Brett Sutton <bsutton@noojee.com.au>
2021-01-03 14:52:27 +1100

    exposed Git as part of the public api.

--- lib/pub_release.dart
+++ lib/pub_release.dart
@@ -4,5 +4,6 @@ export 'src/git_hub_release.dart';
 export 'src/version/version.dart';
 export 'src/create_release.dart';
 export 'src/pubspec_helper.dart';
+export 'src/git.dart';
 
 export 'package:pub_semver/pub_semver.dart' show Version;
