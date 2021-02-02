0de46979932964ea8b5577553d7e1ced20a6e0b3
Brett Sutton <bsutton@noojee.com.au>
2021-02-02 16:56:40 +1100

    restored test code to working state.

--- test/git_hub_release_test.dart
+++ test/git_hub_release_test.dart
@@ -1,59 +1,66 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:dcli/dcli.dart';
-// import 'package:mime/mime.dart';
-// import 'package:pub_release/src/git_hub_release.dart';
+import 'package:dcli/dcli.dart';
+import 'package:mime/mime.dart';
+import 'package:pub_release/src/simple_github.dart';
+import 'package:settings_yaml/settings_yaml.dart';
 
 void main() {
-//   var ghr = GitHubRelease(
-//       username: 'bsutton',
-//       apiToken: 'XXXXX',
-//       owner: 'bsutton',
-//       repository: 'dcli');
+  final settingsPath = truepath(join('test', 'settings.yaml'));
+  print('loading settings from $settingsPath');
 
-//   ghr.auth();
+  final settings = SettingsYaml.load(pathToSettings: settingsPath);
 
-//   var tagName = '0.0.3-${Platform.operatingSystem}';
+  final ghr = SimpleGitHub(
+      username: settings['username'] as String,
+      apiToken: settings['apiToken'] as String,
+      owner: settings['owner'] as String,
+      repository: 'dcli');
 
-//   /// update latest tag to point to this new tag.
-//   var old = ghr.getByTagName(tagName: tagName);
-//   if (old != null) {
-//     print('replacing release $tagName');
-//     ghr.deleteRelease(old);
-//   }
+  ghr.auth();
 
-//   // 'dcli compile -o $HOME/git/dcli/bin/dcli_install.dart'.run;
-//   var exe = '$HOME/git/dcli/bin/dcli_install';
-//   print('Creating release: $tagName');
-//   var release = ghr.release(tagName: tagName);
+  const tagName = '0.0.3-test';
 
-// // 'application/vnd.microsoft.portable-executable'
-//   print('Sending Asset  $exe');
-//   ghr.attachAssetFromFile(
-//     release: release,
-//     assetPath: exe,
-//     assetName: 'dcli_install',
-//     // assetLabel: 'DCli installer',
-//     mimeType: lookupMimeType('$exe.exe'),
-//   );
-//   print('send complete');
+  /// update latest tag to point to this new tag.
+  final old = ghr.getByTagName(tagName: tagName);
+  if (old != null) {
+    print('replacing release $tagName');
+    ghr.deleteRelease(old);
+  }
 
-//   /// update latest tag to point to this new tag.
-//   var latest = ghr.getByTagName(tagName: 'latest-${Platform.operatingSystem}');
-//   if (latest != null) {
-//     ghr.deleteRelease(latest);
-//   }
+  // 'dcli compile -o $HOME/git/dcli/bin/dcli_install.dart'.run;
+  final exe = '$HOME/git/dcli/bin/dcli_install';
+  print('Creating release: $tagName');
+  var release = ghr.release(tagName: tagName);
 
-//   release = ghr.release(tagName: 'latest-${Platform.operatingSystem}');
+// 'application/vnd.microsoft.portable-executable'
+  print('Sending Asset  $exe');
+  ghr.attachAssetFromFile(
+    release: release,
+    assetPath: exe,
+    assetName: 'dcli_install',
+    // assetLabel: 'DCli installer',
+    mimeType: lookupMimeType('$exe.exe'),
+  );
+  print('send complete');
 
-// // 'application/vnd.microsoft.portable-executable'
-//   print('Sending Asset');
-//   ghr.attachAssetFromFile(
-//     release: release,
-//     assetPath: exe,
-//     assetName: 'dcli_install',
-//     // assetLabel: 'DCli installer',
-//     mimeType: lookupMimeType('$exe.exe'),
-//   );
-//   print('send complete');
+  /// update latest tag to point to this new tag.
+  final latest =
+      ghr.getByTagName(tagName: 'latest-${Platform.operatingSystem}');
+  if (latest != null) {
+    ghr.deleteRelease(latest);
+  }
+
+  release = ghr.release(tagName: 'latest-${Platform.operatingSystem}');
+
+// 'application/vnd.microsoft.portable-executable'
+  print('Sending Asset');
+  ghr.attachAssetFromFile(
+    release: release,
+    assetPath: exe,
+    assetName: 'dcli_install',
+    // assetLabel: 'DCli installer',
+    mimeType: lookupMimeType('$exe.exe'),
+  );
+  print('send complete');
 }
