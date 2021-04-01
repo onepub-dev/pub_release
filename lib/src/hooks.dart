8ede3bcf8d583d9550a3850d438c76b8cf0f9ad4
Brett Sutton <bsutton@noojee.com.au>
2021-04-02 07:07:55 +1100

    Added list of extensions to ignore if found in the hook directory.

--- lib/src/hooks.dart
+++ lib/src/hooks.dart
@@ -4,15 +4,17 @@ import 'package:pub_semver/pub_semver.dart';
 /// Checks that all hooks are marked as executable
 void checkHooksAreReadyToRun(String pathToPackageRoot) {
   for (final hook in getHooks(preReleaseRoot(pathToPackageRoot))) {
+    if (_isIgnoredFile(hook)) continue;
     if (!isExecutable(hook)) {
-      print(red('Found non-executable hook: ${truepath(hook)}'));
+      print(red('Ignored non-executable hook: ${truepath(hook)}'));
       print('Remove the hook or mark it as executable');
     }
   }
 
   for (final hook in getHooks(postReleaseRoot(pathToPackageRoot))) {
+    if (_isIgnoredFile(hook)) continue;
     if (!isExecutable(hook)) {
-      print(red('Found non-executable hook: ${truepath(hook)}'));
+      print(red('Ignored non-executable hook: ${truepath(hook)}'));
       print('Remove the hook or mark it as executable');
     }
   }
@@ -26,6 +28,7 @@ void runPreReleaseHooks(String pathToPackageRoot, {Version? version}) {
   var ran = false;
   if (exists(root)) {
     for (final hook in getHooks(root)) {
+      if (_isIgnoredFile(hook)) continue;
       if (isExecutable(hook)) {
         print(blue('Running pre hook: ${basename(hook)}'));
         '$hook ${version.toString()}'.run;
@@ -40,6 +43,13 @@ void runPreReleaseHooks(String pathToPackageRoot, {Version? version}) {
   }
 }
 
+const _ignoredExtensions = ['.yaml', '.ini', '.config'];
+bool _isIgnoredFile(String pathToHook) {
+  final _extension = extension(pathToHook);
+
+  return _ignoredExtensions.contains(_extension);
+}
+
 /// looks for any scripts in the packages tool/post_release_hook directory
 /// and runs them all in alpha numeric order
 void runPostReleaseHooks(String pathToPackageRoot, {Version? version}) {
@@ -48,6 +58,7 @@ void runPostReleaseHooks(String pathToPackageRoot, {Version? version}) {
   var ran = false;
   if (exists(root)) {
     for (final hook in getHooks(root)) {
+      if (_isIgnoredFile(hook)) continue;
       print(blue('Running post hook: ${basename(hook)}'));
       '$hook ${version.toString()}'.run;
       ran = true;
