aa1d24959f4415ce525f07df97ee796fca2edd1c
Brett Sutton <bsutton@noojee.com.au>
2021-05-14 15:02:19 +1000

    renamed pubsem to multiSettings.

--- lib/src/overrides.dart
+++ lib/src/overrides.dart
@@ -7,44 +7,46 @@ import 'multi_settings.dart';
 ///
 
 /// Removes all of the dependency_overrides for each of the packages
-/// listed in the pubsem.yaml file.
+/// listed in the pubrelease_multi.yaml file.
 void removeOverrides(String pathToProjectRoot) {
-  final pubsem = MultiSettings()..load();
+  final multiSettings = MultiSettings()..load();
 
-  for (final package in pubsem.packages) {
+  for (final package in multiSettings.packages) {
     final pubspecPath = join(pathToProjectRoot, package.path, 'pubspec.yaml');
     final pubspec = PubSpec.fromFile(pubspecPath);
-    _removeOverrides(pubspec, pubsem);
+    _removeOverrides(pubspec, multiSettings);
     pubspec.saveToFile(pubspecPath);
   }
 }
 
+/// Adds all of the overrides required by the [MultiSettings] config
+/// file.
 void addOverrides(String pathToProjectRoot) {
-  final pubsem = MultiSettings()..load();
+  final multiSettings = MultiSettings()..load();
 
-  for (final package in pubsem.packages) {
+  for (final package in multiSettings.packages) {
     final pubspecPath = join(pathToProjectRoot, package.path, 'pubspec.yaml');
     final pubspec = PubSpec.fromFile(pubspecPath);
 
     /// remove and re-add overrides in case they have changed.
-    _removeOverrides(pubspec, pubsem);
-    _addOverrides(pathToProjectRoot, pubspec, pubsem);
+    _removeOverrides(pubspec, multiSettings);
+    _addOverrides(pathToProjectRoot, pubspec, multiSettings);
 
     pubspec.saveToFile(pubspecPath);
   }
 }
 
-/// Adds the set of packages in [pubsem] into [pubspec].
-/// Assumes that the packages don't already exists in [pubsem]
+/// Adds the set of packages in [MultiSettings] into [pubspec].
+/// Assumes that the packages don't already exists in [multiSettings]
 ///
 void _addOverrides(
-    String pathToProjectRoot, PubSpec pubspec, MultiSettings pubsem) {
+    String pathToProjectRoot, PubSpec pubspec, MultiSettings multiSettings) {
   final updated = Map<String, Dependency>.from(pubspec.dependencyOverrides);
-  for (final package in pubsem.packages) {
+  for (final package in multiSettings.packages) {
     // we don't add an override to ourselves
     if (package.name == pubspec.name) continue;
 
-    /// we only add an override if we are already depending.
+    /// we only add an override if the pubspec has an existing dependency on [package.name]
     if (!pubspec.dependencies.containsKey(package.name)) continue;
     final dep = Dependency.fromPath(
         package.name, relative(package.path, from: pathToProjectRoot));
@@ -55,11 +57,11 @@ void _addOverrides(
 }
 
 /// Removes any overrides that related to packages found in
-/// [pubsem].
-void _removeOverrides(PubSpec pubspec, MultiSettings pubsem) {
+/// [multiSettings].
+void _removeOverrides(PubSpec pubspec, MultiSettings multiSettings) {
   final keep = <String, Dependency>{};
   for (final entry in pubspec.dependencyOverrides.entries) {
-    if (!pubsem.containsPackage(entry.key)) {
+    if (!multiSettings.containsPackage(entry.key)) {
       keep[entry.key] = entry.value;
     }
   }
