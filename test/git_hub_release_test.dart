4dbe87be2699cf67f371b7d6304cd1051d54e3a9
Brett Sutton <bsutton@noojee.com.au>
2023-11-30 10:24:55 +1100

    Fixed a bug in the git_hub_release_test as it was using the sync version of withTempDir to call an async function which don't work.

--- test/git_hub_release_test.dart
+++ test/git_hub_release_test.dart
@@ -1,36 +1,36 @@
 import 'dart:io';
 
-import 'package:dcli/dcli.dart';
+import 'package:dcli/dcli.dart' hide Settings;
+import 'package:dcli_core/dcli_core.dart' as core;
 import 'package:mime/mime.dart';
 import 'package:path/path.dart';
-import 'package:pub_release/src/simple_github.dart';
-import 'package:settings_yaml/settings_yaml.dart';
+import 'package:pub_release/pub_release.dart';
 
 void main() async {
   final settingsPath = truepath(join('test', 'settings.yaml'));
   print('loading settings from $settingsPath');
 
-  final settings = SettingsYaml.load(pathToSettings: settingsPath);
+  final settings = Settings.loadFromPath(pathToSettings: settingsPath);
 
-  if (settings['username'] == null) {
+  if (settings.username == null) {
     print(red('username not set in settings.yaml'));
     exit(1);
   }
 
-  if (settings['apiToken'] == null) {
+  if (settings.apiToken == null) {
     print(red('apiToken not set in settings.yaml'));
     exit(1);
   }
 
-  if (settings['owner'] == null) {
+  if (settings.owner == null) {
     print(red('owner not set in settings.yaml'));
     exit(1);
   }
 
   final ghr = SimpleGitHub(
-      username: settings['username'] as String,
-      apiToken: settings['apiToken'] as String,
-      owner: settings['owner'] as String,
+      username: settings.username!,
+      apiToken: settings.apiToken!,
+      owner: settings.owner!,
       repository: 'dcli')
     ..auth();
 
@@ -46,7 +46,7 @@ void main() async {
     print('release not found');
   }
 
-  await withTempDir((tempDir) async {
+  await core.withTempDir((tempDir) async {
     final pathToProject = join(tempDir, 'aproject');
     final project =
         DartProject.create(pathTo: pathToProject, templateName: 'simple');
