555af32ad6b03ba7359a74a1771f183ce014f527
Brett Sutton <bsutton@noojee.com.au>
2024-07-31 14:17:08 +1000

    Added an exception handler in main for PubReleaseExceptions so we print the error rather than dumping a stack trace.

--- bin/pub_release.dart
+++ bin/pub_release.dart
@@ -15,69 +15,7 @@ import 'package:pub_release/src/multi_release.dart';
 import 'package:pub_release/src/version/version.g.dart';
 
 void main(List<String> args) {
-  final parser = ArgParser()
-    ..addFlag('askVersion',
-        abbr: 'k',
-        defaultsTo: true,
-        negatable: false,
-        help: 'Prompts the user for the new version no.')
-    ..addOption('setVersion',
-        abbr: 's',
-        help: 'Allows you to set the version no. from the cli. '
-            '--setVersion=1.0.0')
-    ..addFlag('autoAnswer',
-        abbr: 'a', help: 'Supresses any questions from being asked.')
-    ..addFlag('dry-run',
-        abbr: 'd',
-        negatable: false,
-        help: 'Validate but do not publish the package.')
-    ..addFlag('test',
-        abbr: 't', defaultsTo: true, help: 'Runs the package(s) unit tests.')
-
-    // parser.addFlag('runfailed',
-    //     abbr: 'f', help: 'Reruns unit tests that failed on a prior run.');
-
-    ..addOption('line',
-        abbr: 'l',
-        defaultsTo: '80',
-        help: 'Specifies the line length to use when formatting.')
-    ..addFlag(
-      'format',
-      abbr: 'f',
-      defaultsTo: true,
-      help: '''
-Allows you to control whether code is formatted as part of the relase.
-Use --no-format to supress formatting.
-''',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Outputs detailed logging.',
-    )
-    ..addOption('tags',
-        abbr: 'g',
-        help:
-            'Select unit tests to run via their tags. The syntax must confirm '
-            'to the --tags option in the test package.')
-    ..addFlag('git',
-        abbr: 'i',
-        defaultsTo: true,
-        help: 'Controls whether git operations are performed as part of the '
-            'release.')
-    ..addOption('exclude-tags',
-        abbr: 'x',
-        help: 'Select unit tests to exclude via their tags. '
-            'The syntax must confirm to the --exclude-tags option '
-            'in the test package.')
-    ..addFlag('no-multi',
-        abbr: 'm',
-        negatable: false,
-        help: 'Use --no-multi to disable a multi-project build when you have a '
-            'pubrelease.multi.yaml file')
-    ..addCommand('help')
-    ..addCommand('multi');
+  final parser = _buildParser();
 
   late final ArgResults results;
   try {
@@ -88,118 +26,123 @@ Use --no-format to supress formatting.
     results = parser.parse(['help']);
   }
 
-  final settings = pb.Settings.load();
+  try {
+    final settings = pb.Settings.load();
 
-  final dryrun = results['dry-run'] as bool;
-  final useGit = results['git'] as bool;
-  final runTests = results['test'] as bool;
-  final autoAnswer = results['autoAnswer'] as bool;
-  final verbose = results['verbose'] as bool;
+    final dryrun = results['dry-run'] as bool;
+    final useGit = results['git'] as bool;
+    final runTests = results['test'] as bool;
+    final autoAnswer = results['autoAnswer'] as bool;
+    final verbose = results['verbose'] as bool;
 
-  var format = settings.format;
-  if (results.wasParsed('format')) {
-    format = results['format'] as bool;
-  }
-
-  final noMulti = results['no-multi'] as bool;
-
-  Settings().setVerbose(enabled: verbose);
-
-  var multi = false;
-  // was a command passed
-  if (results.command != null) {
-    switch (results.command!.name) {
-      case 'help':
-        showUsage(parser);
-        exit(1);
-      case 'multi':
-        multi = true;
-        break;
+    var format = settings.format;
+    if (results.wasParsed('format')) {
+      format = results['format'] as bool;
     }
-  }
 
-  checkMultiFlags(multi: multi, noMulti: noMulti);
+    final noMulti = results['no-multi'] as bool;
 
-  print('${DartScript.self.basename} $packageVersion');
+    Settings().setVerbose(enabled: verbose);
 
-  final lineLength = getLineLength(results, parser);
+    var multi = false;
+    // was a command passed
+    if (results.command != null) {
+      switch (results.command!.name) {
+        case 'help':
+          showUsage(parser);
+          exit(1);
+        case 'multi':
+          multi = true;
+          break;
+      }
+    }
 
-  if (results.wasParsed('askVersion') && results.wasParsed('setVersion')) {
-    printerr(red('You may only pass one of "setVersion" or "askVersion"'));
-    showUsage(parser);
-    exit(1);
-  }
+    checkMultiFlags(multi: multi, noMulti: noMulti);
 
-  /// determine how the version will be set.
-  var versionMethod = VersionMethod.ask;
-  Version? parsedVersion;
-  if (results.wasParsed('setVersion')) {
-    versionMethod = VersionMethod.set;
-    final version = results['setVersion'] as String;
-    try {
-      parsedVersion = Version.parse(version);
-    } on FormatException catch (_) {
-      printerr(red('The version no. "$version" passed to setVersion is not '
-          'a valid version.'));
+    print('${DartScript.self.basename} $packageVersion');
+
+    final lineLength = getLineLength(results, parser);
+
+    if (results.wasParsed('askVersion') && results.wasParsed('setVersion')) {
+      printerr(red('You may only pass one of "setVersion" or "askVersion"'));
+      showUsage(parser);
       exit(1);
     }
-  }
 
-  if (autoAnswer && versionMethod != VersionMethod.set) {
-    printerr(red(
-        'When using --autoAnswer you must also pass --setVersion=<version>'));
-    exit(1);
-  }
-
-  String? tags;
-  if (results.wasParsed('tags')) {
-    tags = results['tags'] as String;
-  }
-
-  String? excludeTags;
-  if (results.wasParsed('exclude-tags')) {
-    excludeTags = results['exclude-tags'] as String;
-  }
-
-  try {
-    if (multi) {
-      multiRelease(DartProject.fromPath(pwd).pathToProjectRoot, versionMethod,
-          parsedVersion,
-          lineLength: lineLength,
-          format: format,
-          dryrun: dryrun,
-          runTests: runTests,
-          autoAnswer: autoAnswer,
-          tags: tags,
-          excludeTags: excludeTags,
-          useGit: useGit);
-    } else {
-      final runner = ReleaseRunner(pwd);
-      final pubspecDetails = runner.checkPackage(autoAnswer: autoAnswer);
-
-      runner.pubRelease(
-          pubSpecDetails: pubspecDetails,
-          versionMethod: versionMethod,
-          setVersion: parsedVersion,
-          lineLength: lineLength,
-          format: format,
-          dryrun: dryrun,
-          runTests: runTests,
-          autoAnswer: autoAnswer,
-          tags: tags,
-          excludeTags: excludeTags,
-          useGit: useGit);
+    /// determine how the version will be set.
+    var versionMethod = VersionMethod.ask;
+    Version? parsedVersion;
+    if (results.wasParsed('setVersion')) {
+      versionMethod = VersionMethod.set;
+      final version = results['setVersion'] as String;
+      try {
+        parsedVersion = Version.parse(version);
+      } on FormatException catch (_) {
+        printerr(red('The version no. "$version" passed to setVersion is not '
+            'a valid version.'));
+        exit(1);
+      }
     }
-  } on UnitTestFailedException catch (e) {
+
+    if (autoAnswer && versionMethod != VersionMethod.set) {
+      printerr(red(
+          'When using --autoAnswer you must also pass --setVersion=<version>'));
+      exit(1);
+    }
+
+    String? tags;
+    if (results.wasParsed('tags')) {
+      tags = results['tags'] as String;
+    }
+
+    String? excludeTags;
+    if (results.wasParsed('exclude-tags')) {
+      excludeTags = results['exclude-tags'] as String;
+    }
+
+    try {
+      if (multi) {
+        multiRelease(DartProject.fromPath(pwd).pathToProjectRoot, versionMethod,
+            parsedVersion,
+            lineLength: lineLength,
+            format: format,
+            dryrun: dryrun,
+            runTests: runTests,
+            autoAnswer: autoAnswer,
+            tags: tags,
+            excludeTags: excludeTags,
+            useGit: useGit);
+      } else {
+        final runner = ReleaseRunner(pwd);
+        final pubspecDetails = runner.checkPackage(autoAnswer: autoAnswer);
+
+        runner.pubRelease(
+            pubSpecDetails: pubspecDetails,
+            versionMethod: versionMethod,
+            setVersion: parsedVersion,
+            lineLength: lineLength,
+            format: format,
+            dryrun: dryrun,
+            runTests: runTests,
+            autoAnswer: autoAnswer,
+            tags: tags,
+            excludeTags: excludeTags,
+            useGit: useGit);
+      }
+    } on UnitTestFailedException catch (e) {
+      print('');
+      print(e.message);
+      exit(1);
+    }
     print('');
-    print(e.message);
+    if (dryrun) {
+      print(blue('Dry run suceeded. You packages are ready to release'));
+    } else {
+      print(blue('Your packages have been published.'));
+    }
+  } on pb.PubReleaseException catch (e) {
+    printerr(red(e.message));
     exit(1);
-  }
-  print('');
-  if (dryrun) {
-    print(blue('Dry run suceeded. You packages are ready to release'));
-  } else {
-    print(blue('Your packages have been published.'));
   }
 }
 
@@ -285,3 +228,66 @@ pub_release [multi|help] [--dry-run] [--[no]-test] [--line=nn] [--askVersion|--s
 ${parser.usage}
       ''');
 }
+
+ArgParser _buildParser() => ArgParser()
+  ..addFlag('askVersion',
+      abbr: 'k',
+      defaultsTo: true,
+      negatable: false,
+      help: 'Prompts the user for the new version no.')
+  ..addOption('setVersion',
+      abbr: 's',
+      help: 'Allows you to set the version no. from the cli. '
+          '--setVersion=1.0.0')
+  ..addFlag('autoAnswer',
+      abbr: 'a', help: 'Supresses any questions from being asked.')
+  ..addFlag('dry-run',
+      abbr: 'd',
+      negatable: false,
+      help: 'Validate but do not publish the package.')
+  ..addFlag('test',
+      abbr: 't', defaultsTo: true, help: 'Runs the package(s) unit tests.')
+
+  // parser.addFlag('runfailed',
+  //     abbr: 'f', help: 'Reruns unit tests that failed on a prior run.');
+
+  ..addOption('line',
+      abbr: 'l',
+      defaultsTo: '80',
+      help: 'Specifies the line length to use when formatting.')
+  ..addFlag(
+    'format',
+    abbr: 'f',
+    defaultsTo: true,
+    help: '''
+Allows you to control whether code is formatted as part of the relase.
+Use --no-format to supress formatting.
+''',
+  )
+  ..addFlag(
+    'verbose',
+    abbr: 'v',
+    negatable: false,
+    help: 'Outputs detailed logging.',
+  )
+  ..addOption('tags',
+      abbr: 'g',
+      help: 'Select unit tests to run via their tags. The syntax must confirm '
+          'to the --tags option in the test package.')
+  ..addFlag('git',
+      abbr: 'i',
+      defaultsTo: true,
+      help: 'Controls whether git operations are performed as part of the '
+          'release.')
+  ..addOption('exclude-tags',
+      abbr: 'x',
+      help: 'Select unit tests to exclude via their tags. '
+          'The syntax must confirm to the --exclude-tags option '
+          'in the test package.')
+  ..addFlag('no-multi',
+      abbr: 'm',
+      negatable: false,
+      help: 'Use --no-multi to disable a multi-project build when you have a '
+          'pubrelease.multi.yaml file')
+  ..addCommand('help')
+  ..addCommand('multi');
